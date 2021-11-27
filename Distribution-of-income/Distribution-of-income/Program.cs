00e474a88e9a60593a21664d514be2c6f9ece0ed
Wolfrein <tigr5557@gmail.com>
2021-11-27 14:01:47 +0300

    Made some chenges

--- Distribution-of-income/Distribution-of-income/Program.cs
+++ Distribution-of-income/Distribution-of-income/Program.cs
@@ -7,25 +7,49 @@ namespace Distribution_of_income
         static void Main(string[] args)
         {
             string summa;
-            Console.Write("Enter the amount: ");
+            Console.Write("(З/П) Enter the amount: ");
             summa = Console.ReadLine();
+            decimal num;
+
+                if (!decimal.TryParse(summa, out num))
+                {
+                    Console.WriteLine("You entered incorrect data");
+                    return;
+                }
+
+            decimal amount = Convert.ToDecimal(summa);
+
             Console.WriteLine(" ");
-            int amount = Convert.ToInt32(summa);
+           
             decimal tenPercent = (amount * 10m) / 100;
             decimal saving = (amount / 2m) - tenPercent;
-            decimal petrol = 50m;
+            decimal communal = 60m;
+            decimal petrol = 60m;
             decimal coupons = 30m;
             decimal phone = 40m;
             decimal doctor = 150m;
+           
             decimal remainder = amount - tenPercent - saving - petrol - coupons - phone - doctor;
+            decimal consumption = communal + petrol + coupons + phone + doctor;
 
-            Console.WriteLine($" 10 percent of income: {tenPercent}\n");
-            Console.WriteLine($" Saving: {saving}\n");
-            Console.WriteLine($" Petrol: {petrol}\n");
-            Console.WriteLine($" Coupons: {coupons}\n");
-            Console.WriteLine($" Phone: {phone}\n");
-            Console.WriteLine($" Doctor: {doctor}\n");
-            Console.WriteLine($" Remainder: {remainder}\n");
+            Console.WriteLine($" (10%) 10 percent of income:  {Math.Round(tenPercent,2) } BYR\n");
+            Console.WriteLine($" (Сбережения) Saving: {Math.Round(saving, 2)} BYR\n");
+            Console.WriteLine($" (Коммунальные) Communal: {communal} BYR\n");
+            Console.WriteLine($" (Бензин) Petrol: {petrol} BYR\n");
+            Console.WriteLine($" (Талоны) Coupons: {coupons} BYR\n");
+            Console.WriteLine($" (Тел.) Phone: {phone} BYR\n");
+            Console.WriteLine($" (Врач) Doctor: {doctor} BYR\n");
+            Console.WriteLine($" (Общ. расход) Total expense: {consumption} BYR\n");
+           
+           
+            if(remainder <0)
+                Console.WriteLine($" (Не хватает) Lasks: {Math.Round(remainder, 2)} BYR\n");
+            else
+                Console.WriteLine($" (Остаток) Remainder: {Math.Round(remainder, 2)} BYR\n");
+            while (true)
+            Console.ReadLine();
         }
+        
     }
+    
 }
